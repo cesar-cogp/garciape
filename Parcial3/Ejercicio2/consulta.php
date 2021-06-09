b734d945548a8dc798b1289894e45023f140eadc
cesar-cogp <l18100176@nlaredo.tecnm.mx>
2021-06-09 02:56:34 -0500

    ConsultaPHPw/PDO

--- Parcial3/Ejercicio2/consulta.php
+++ Parcial3/Ejercicio2/consulta.php
@@ -2,7 +2,7 @@
     include('cn.php');
 
     try{
-        $query = "SELECT * FROM mercancia";
+        $query = "select * from mercancia";
         $statement = $conn->prepare($query);
         $statement -> setFetchMode(PDO::FETCH_ASSOC);
         $data = array();
@@ -11,7 +11,6 @@
         // Ciclo PDO que sera en encargado de mostrar los registros de la BD
             while($row = $statement->fetch())
                 {
-                    //$data[] = $row;
                     printf("<pre>Codigo: ".$row["codigo"]."<br>");
                     printf("Tipo: ".$row["tipo"]."<br>");
                     printf("Marca: ".$row["marca"]."<br>");
@@ -22,8 +21,6 @@
                     printf("Precio Unitario: ".$row["precio_unitario"]."<br>");
                     printf("Estatus: ".$row["estatus"]."</pre><br>");
                 }
-
-            echo json_encode($data);
         }
     }catch(PDOException $pe){
         die("Ocurrio un error en la consulta.".$pe->getMessage());
